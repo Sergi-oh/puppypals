7f582ee88398e1ded639c617c23cf68276f9683f
Sergi-oh <sehernan96@gmail.com>
2023-09-05 23:22:31 -0700

    attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,21 @@
-<div></div>
+import {puppyList} from './data.js'
+import { useState } from 'react'
+console.log(puppyList);
+
+function App() {
  
+
+  return (
+    const [puppies, setPuppies] = useState(puppyList)
+      <div>
+        { 
+   puppies.map((puppy) => {
+     return <p>{puppy.name}</p>
+   })
+}
+         </div>
+      
+  )
+}
+
+export default App
